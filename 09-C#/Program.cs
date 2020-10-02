af3dd2dbbb6cdd48155d24efb0de8cfac15cb4bf
firdausyah22 <fillahfirdausyah22@gmail.com>
2020-10-03 01:09:02 +0700

    Hello World in C#

---
+++ 09-C#/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _09_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
