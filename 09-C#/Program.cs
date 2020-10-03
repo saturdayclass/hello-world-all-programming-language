479f706cc80e98be92117b5cd34bd4cc106a9d61
Ronny Gunawan <3048897+ronnygunawan@users.noreply.github.com>
2020-10-03 14:39:31 +0700

    Retarget to .NET 5.0, remove obj folder, add standard .NET .gitignore

--- 09-C#/Program.cs
+++ 09-C#/Program.cs
@@ -1,12 +1,3 @@
 ﻿using System;
 
-namespace _09_C_
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+Console.WriteLine("Hello World!");
